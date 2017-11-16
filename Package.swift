4cef026597e974d98b43906b005aa89ec76e3da7
Rodrigo Kreutz <rodrigo.kreutz@acad.pucrs.br>
2017-11-16 11:01:01 -0200

    Cleaning Template project

--- Package.swift
+++ Package.swift
@@ -11,16 +11,19 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "2.1.0")),
         .package(url: "https://github.com/vapor/fluent-provider.git", .upToNextMajor(from: "1.2.0")),
+        .package(url: "https://github.com/vapor/auth-provider.git", .upToNextMajor(from: "1.0.0")),
+        .package(url: "https://github.com/vapor/jwt-provider.git", .upToNextMajor(from: "1.0.0")),
+        .package(url: "https://github.com/vapor/validation-provider.git", .upToNextMajor(from: "1.0.0")),
+        .package(url: "https://github.com/vapor-community/mongo-provider.git", .upToNextMajor(from: "2.0.0")),
     ],
     targets: [
-        .target(name: "App", dependencies: ["Vapor", "FluentProvider"],
+        .target(name: "App", dependencies: ["Vapor", "FluentProvider", "AuthProvider", "JWTProvider", "ValidationProvider", "MongoProvider"],
                 exclude: [
                     "Config",
                     "Public",
                     "Resources",
                 ]),
-        .target(name: "Run", dependencies: ["App"]),
-        .testTarget(name: "AppTests", dependencies: ["App", "Testing"])
+        .target(name: "Run", dependencies: ["App"])
     ]
 )
 
