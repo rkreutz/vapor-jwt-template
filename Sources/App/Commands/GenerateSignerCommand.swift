6d13884a1f7bb21d87f9bfa37a3017080cd30cea
Rodrigo Kreutz <rodrigo.kreutz@acad.pucrs.br>
2017-11-17 19:53:18 -0200

    Generating signer using command, accepting bits argument

--- Sources/App/Commands/GenerateSignerCommand.swift
+++ Sources/App/Commands/GenerateSignerCommand.swift
@@ -19,18 +19,34 @@ final class GenerateSignerCommand: Command {
     }
     
     public func run(arguments: [String]) throws {
+        var bits = "4096"
         try arguments.forEach { (argument) in
             guard argument.hasPrefix("--"), argument.count > 2 else { throw CommandError.general("Invalid argument: \"\(argument)\"") }
-            let keyValuePair = argument.suffix(from: argument.index(argument.startIndex, offsetBy: 2)).split(separator: "=")
+            let keyValuePair = argument[argument.index(argument.startIndex, offsetBy: 2)...].split(separator: "=")
             guard let key = keyValuePair.first, let value = keyValuePair.last, keyValuePair.count == 2 else { throw CommandError.general("Invalid argument: \"\(argument)\"") }
+            if key == "bits" {
+                bits = String(value)
+            } else {
+                throw CommandError.general("Unsupported key: \(key)")
+            }
         }
         
         var baseUrl = try self.fileManager.url(for: .cachesDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
-        baseUrl.appendPathComponent("rsa")
+        baseUrl.appendPathComponent("tmp")
         guard self.fileManager.createFile(atPath: baseUrl.path, contents: nil, attributes: nil) else { throw CommandError.general("Certificate file couldn't be created/found.") }
-        print(baseUrl.path)
-        let file = try FileHandle.init(forWritingTo: baseUrl)
-        try console.execute(program: "openssl", arguments: ["genrsa", "512"], input: nil, output: file.fileDescriptor, error: nil)
+        let writeHandle = try FileHandle.init(forWritingTo: baseUrl)
+        let readHandle = try FileHandle.init(forReadingFrom: baseUrl)
+        
+        try console.execute(program: "openssl", arguments: ["genrsa", bits], input: nil, output: writeHandle.fileDescriptor, error: nil)
+        
+        let data = readHandle.readDataToEndOfFile()
+        guard var key = String(data: data, encoding: .utf8) else { throw CommandError.general("Key generated wasn't valid") }
+        key = key.replacingOccurrences(of: "\n", with: "")
+        key = key.replacingOccurrences(of: "-{5}\\D{0,30}-{5}", with: "", options: .regularExpression)
+        
+        console.success("Add the following signer to your 'jwt.json' file:\n\"signerName\": {\n\t\"type\": \"rsa\",\n\t\"algorithm\": \"rs256\",\n\t\"key\": \"\(key)\"\n}", newLine: true)
+        
+        try self.fileManager.removeItem(at: baseUrl)
     }
 }
 
